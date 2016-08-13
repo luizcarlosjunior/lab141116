00562d9528f0907bcef45c4d9052ba672ba16a3c
Luiz Carlos Szpikula Junior <luiz.szpikula@alfaconcursos.com.br>
2016-08-13 18:28:52 -0300

    #2 - criado a classe VendaServlet extends HttpServlet - criado a classe EntregaServlet extends HttpServlet

---
+++ src/main/java/servlet/EntregaServlet.java
@@ -0,0 +1,22 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/entrega")
+public class EntregaServlet extends HttpServlet{
+
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.write("<h1>Olá você está na rota /entrega</h1>");
+
+    }
+}
