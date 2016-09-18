93136ba3cba877b4d23c2a73d83741f1507378d5
Luiz Carlos Szpikula Junior <luiz.szpikula@alfaconcursos.com.br>
2016-09-18 19:29:12 -0300

    #9

---
+++ src/main/java/jaxWS/EntregaWS.java
@@ -0,0 +1,62 @@
+package jaxWS;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import javax.ejb.EJB;
+import javax.jws.WebMethod;
+import javax.jws.WebResult;
+import javax.jws.WebService;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ejb.ProcessEntrega;
+import model.Entrega;
+import model.Venda;
+
+@WebService
+public class EntregaWS{
+
+	@EJB
+	ProcessEntrega EJB_ProcessEntrega;
+
+	@WebMethod(operationName="entregar")
+	@WebResult(name="resultado")
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+		// venda fake :P
+
+		// itens
+		List<String> itens = Arrays.asList("item 1", "item 2", "item 3");
+
+		//venda
+		Venda venda = new Venda();
+		venda.setId(1);
+		venda.setData(new Date());
+		venda.setCliente_nome("João Sem Braço");
+		venda.setCliente_endereco("Rua X, n 0");
+		venda.setCliente_cpf("000.000.000-00");
+		venda.setStatus(1);
+		venda.setItens(itens);
+		venda.setQtd_itens(itens.size());
+		venda.setValor(100.00);
+
+		//entrega fake
+		Entrega entrega = new Entrega();
+		entrega.setId(1);
+		entrega.setData(new Date());
+		entrega.setVenda(venda);
+		entrega.setStatus(1);
+
+		//enviar para o EJB
+		EJB_ProcessEntrega.processarEntrega(entrega);
+
+		resp.setContentType("text/html");
+	    PrintWriter out = resp.getWriter();
+	    out.write("<h1>Olá você está na rota /entrega</h1>");
+	    out.write("<h2>A venda número '#" + entrega.getId() + "' do Cliente '" + entrega.getVenda().getCliente_nome() + "' foi enviada.</h2>");
+    }
+
+}
